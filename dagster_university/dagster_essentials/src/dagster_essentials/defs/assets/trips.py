c4abe8d48fce9eef9ab30e9ccf7599eb12c83594
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2026-09-07 12:28:28 -0300

    taxi zones table

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -8,34 +8,39 @@ import dagster as dg
 
 dotenv.load_dotenv()
 
+
 @dg.asset
 def taxi_trips_file() -> None:
     """
-        The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
-    month_to_fetch = '2023-03'
+    month_to_fetch = "2023-03"
     raw_trips = requests.get(
         f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
     )
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+    with open(
+        constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
+    ) as output_file:
         output_file.write(raw_trips.content)
+
 
 @dg.asset
 def taxi_zones_file() -> None:
     """
-        The raw CSV file for the taxi zones dataset. Sourced form the NYC Open Data portal.
+    The raw CSV file for the taxi zones dataset. Sourced form the NYC Open Data portal.
     """
-    zones = requests.get("https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv")
+    zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(zones.content)
 
-@dg.asset(
-    deps=["taxi_trips_file"]
-)
+
+@dg.asset(deps=["taxi_trips_file"])
 def taxi_trips() -> None:
     """
-      The raw taxi trips dataset, loaded into a DuckDB database
+    The raw taxi trips dataset, loaded into a DuckDB database
     """
     query = """
         create or replace table trips as (
@@ -51,6 +56,33 @@ def taxi_trips() -> None:
             passenger_count as passenger_count,
             total_amount as total_amount
           from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
+
+
+@dg.asset(deps=["taxi_zones_file"])
+def taxi_zones() -> None:
+    """
+    The raw taxi zones dataset, loaded into a DuckDB database
+    """
+    query = """
+        create or replace table zones as (
+          select
+            LocationID as zone_id,
+            zone as zone,
+            borough as borough,
+            the_geom as geometry,
+                    from 'data/raw/taxi_zones.csv'
         );
     """
 
