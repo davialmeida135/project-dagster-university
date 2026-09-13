36d11d675d079b4fffd45ce6a23b434ea4477986
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2026-09-12 23:00:35 -0300

    partitions

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -5,16 +5,18 @@ import requests
 from dagster_essentials.defs.assets import constants
 import dagster as dg
 from dagster_duckdb import DuckDBResource
+from dagster_essentials.defs.partitions import monthly_partition
 
 dotenv.load_dotenv()
 
 
-@dg.asset
-def taxi_trips_file() -> None:
+@dg.asset(partitions_def=monthly_partition)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
     """
     The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
-    month_to_fetch = "2023-03"
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
     raw_trips = requests.get(
         f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
     )
@@ -37,30 +39,39 @@ def taxi_zones_file() -> None:
         output_file.write(zones.content)
 
 
-@dg.asset(deps=["taxi_trips_file"])
-def taxi_trips(database : DuckDBResource) -> None:
+@dg.asset(deps=["taxi_trips_file"], partitions_def=monthly_partition)
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
     """
     The raw taxi trips dataset, loaded into a DuckDB database
     """
-    query = """
-        create or replace table trips as (
-          select
-            VendorID as vendor_id,
-            PULocationID as pickup_zone_id,
-            DOLocationID as dropoff_zone_id,
-            RatecodeID as rate_code_id,
-            payment_type as payment_type,
-            tpep_dropoff_datetime as dropoff_datetime,
-            tpep_pickup_datetime as pickup_datetime,
-            trip_distance as trip_distance,
-            passenger_count as passenger_count,
-            total_amount as total_amount
-          from 'data/raw/taxi_trips_2023-03.parquet'
-        );
+
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    create_query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );"""
+
+    delete_query = f"""
+    delete from trips where partition_date = '{month_to_fetch}';
+    """
+
+    insert_query = f"""
+        insert into trips
+    select
+        VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+        tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+    from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
     """
 
     with database.get_connection() as conn:
-        conn.execute(query)
+        conn.execute(create_query)
+        conn.execute(delete_query)
+        conn.execute(insert_query)
 
 
 @dg.asset(deps=["taxi_zones_file"])
