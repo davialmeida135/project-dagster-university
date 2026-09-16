daca1fd601baecc7c8bad7055032acbbc4ed7e61
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2026-09-16 15:08:00 -0300

    asteroid file

--- dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
+++ dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
@@ -2,67 +2,72 @@ import dagster as dg
 import csv
 from pathlib import Path
 from dagster_duckdb import DuckDBResource
+from dagster_and_etl.defs.resources import NASAResource
+from pydantic import field_validator
+import datetime
+
+# ==================
+# NASA
+# ==================
 
 
-class IngestionFileConfig(dg.Config):
-    path: str = "2018-01-22.csv"
+class NasaDate(dg.Config):
+    date: str
+
+    @field_validator("date")
+    @classmethod
+    def validate_date_format(cls, v):
+        try:
+            datetime.datetime.strptime(v, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("event_date must be in 'YYYY-MM-DD' format")
+        return v
 
 
-@dg.asset()
-def import_file(context: dg.AssetExecutionContext, config: IngestionFileConfig) -> str:
-    file_path = Path(__file__).absolute().parent / f"../../../data/source/{config.path}"
-    return str(file_path.resolve())
+@dg.asset(kinds={"nasa"})
+def asteroids(
+    context: dg.AssetCheckExecutionContext, config: NasaDate, nasa: NASAResource
+) -> list[dict]:
+    anchor_date = datetime.datetime.strptime(config.date, "%Y-%m-%d")
+    start_date = (anchor_date - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-
-@dg.asset(
-    kinds={"duckdb"},
-)
-def duckdb_table(
-    context: dg.AssetExecutionContext,
-    database: DuckDBResource,
-    import_file,
-):
-    table_name = "raw_data"
-    with database.get_connection() as conn:
-        table_query = f"""
-            create table if not exists {table_name} (
-                date date,
-                share_price float,
-                amount float,
-                spend float,
-                shift float,
-                spread float
-            ) 
-        """
-        conn.execute(table_query)
-        conn.execute(f"copy {table_name} from '{import_file}' (format csv, header);")
-
-
-@dg.asset_check(
-    asset=import_file,
-    blocking=True,
-    description="Ensure file contains no zero value shares",
-)
-def not_empty(
-    context: dg.AssetCheckExecutionContext,
-    import_file,
-) -> dg.AssetCheckResult:
-    with open(import_file, mode="r", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        data = (row for row in reader)
-
-        for row in data:
-            if float(row["share_price"]) <= 0:
-                return dg.AssetCheckResult(
-                    passed=False,
-                    metadata={"'share' is below 0": row},
-                )
-
-    return dg.AssetCheckResult(
-        passed=True,
+    return nasa.get_near_earth_asteroids(
+        start_date=start_date,
+        end_date=config.date,
     )
 
+@dg.asset
+def asteroids_file(
+    context: dg.AssetExecutionContext,
+    asteroids,
+) -> Path:
+    filename = "asteroid_staging"
+    file_path = (
+        Path(__file__).absolute().parent / f"../../../data/staging/{filename}.csv"
+    )
 
+    # Only load specific fields
+    fields = [
+        "id",
+        "name",
+        "absolute_magnitude_h",
+        "is_potentially_hazardous_asteroid",
+    ]
+
+    with open(file_path, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=fields)
+
+        writer.writeheader()
+        writer.writerows(
+            {key: row[key] for key in fields if key in row} for row in asteroids
+        )
+
+    return file_path
+
+
+# ==================
+# DAILY PARTITIONS
+# ==================
 partitions_def = dg.DailyPartitionsDefinition(
     start_date="2018-01-22",
     end_date="2018-01-25",
@@ -152,3 +157,28 @@ def duckdb_dynamic_partition_table(
             f"delete from {table_name} where date = '{context.partition_key}';"
         )
         conn.execute(f"copy {table_name} from '{import_dynamic_partition_file}';")
+
+
+@dg.asset_check(
+    asset=import_dynamic_partition_file,
+    blocking=True,
+    description="Ensure file contains no zero value shares",
+)
+def not_empty(
+    context: dg.AssetCheckExecutionContext,
+    import_file,
+) -> dg.AssetCheckResult:
+    with open(import_file, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        data = (row for row in reader)
+
+        for row in data:
+            if float(row["share_price"]) <= 0:
+                return dg.AssetCheckResult(
+                    passed=False,
+                    metadata={"'share' is below 0": row},
+                )
+
+    return dg.AssetCheckResult(
+        passed=True,
+    )
