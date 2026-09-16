5dffbec65a157e2c77f55c5218072ff45a63e9f5
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2026-09-16 14:38:17 -0300

    nasa resource

--- dagster_university/dagster_and_etl/src/dagster_and_etl/defs/resources.py
+++ dagster_university/dagster_and_etl/src/dagster_and_etl/defs/resources.py
@@ -1,5 +1,22 @@
 import dagster as dg
 from dagster_duckdb import DuckDBResource
+import requests
+
+
+class NASAResource(dg.ConfigurableResource):
+    api_key: str
+
+    def get_near_earth_asteroids(self, start_date: str, end_date: str):
+        url = "https://api.nasa.gov/neo/rest/v1/feed"
+        params = {
+            "start_date": start_date,
+            "end_date": end_date,
+            "api_key": self.api_key,
+        }
+
+        resp = requests.get(url, params=params)
+        return resp.json()["near_earth_objects"][start_date]
+
 
 @dg.definitions
 def resources():
@@ -8,5 +25,6 @@ def resources():
             "database": DuckDBResource(
                 database="data/staging/data.duckdb",
             ),
+            "nasa": NASAResource(api_key=dg.EnvVar("NASA_API_KEY")),
         }
     )
